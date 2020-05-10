88c58b7e3fc4176480b8336aaf31a90f763b6a89
Shubham Gupta <shubhamg2595@gmail.com>
2020-05-10 19:04:53 +0530

    React-Todo-App Complete

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from './Container/HomePage'
+
+class App extends Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
+
+export default App;
